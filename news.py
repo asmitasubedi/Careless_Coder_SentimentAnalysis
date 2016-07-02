c6acc660a23f5eb56e4c77c605e3a4aa2f7a5eac
yashinshekh <yashine.shekh@deerwalk.edu.np>
2016-07-02 19:40:29 +0545

    Add files via upload

---
+++ news.py
@@ -0,0 +1,57 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+
+titles = []
+new_title = []
+another_title=[]
+news_link = []
+NEWS=[]
+with urllib.request.urlopen("http://kathmandupost.ekantipur.com/") as url:
+    s = url.read()
+soup = BeautifulSoup(s, 'html.parser')
+
+for link in soup.find_all('a'):
+    if link.has_attr('href'):
+        filter = link.attrs['href']
+        if '/news/2016-07' in filter:
+            news_link.append('http://kathmandupost.ekantipur.com' + filter)
+
+new_title.append(list(set(news_link)))
+
+for i in range(70):
+    print(new_title[0][i])
+
+
+# print(new_title)
+for i in range(1):
+    with urllib.request.urlopen(new_title[0][i]) as url:
+        s = url.read()
+    soup = BeautifulSoup(s, 'html.parser')
+
+    # for link in soup.find_all('h1'):
+    #     # print(link.string)
+    #     titles.append(link.string)
+    #
+    #
+    #     # print(titles[i].string)
+    #
+    #     # with open('mycsv.txt', "w") as output:
+    #     #     writer = csv.writer(output, lineterminator='\n')
+    #     #     writer.writerow(titles[i].string)
+
+    with open('text.txt', 'ab') as f:
+        for link in soup.find_all('p'):
+            f.write(str(link.string))
+    # for link in soup.find_all('p'):
+    #         print(link.string)
+    #         target = open("hellotxt", 'ab')
+    #         target.write(link.string)
+    #         target.close()
+
+            # with open('hello.txt', "w") as output:
+            #     writer = csv.writer(output, lineterminator='\n')
+            #     writer.writerow(link.string)
+            #     for val in link.string:
+            #         writer.writerow([val])
+
