0963ee7f89ee227ef9dcf185bb177ca14840db1e
yashinshekh <yashine.shekh@deerwalk.edu.np>
2016-07-03 14:37:49 +0545

    Add files via upload

--- news.py
+++ news.py
@@ -9,6 +9,7 @@ news_link = []
 NEWS=[]
 with urllib.request.urlopen("http://kathmandupost.ekantipur.com/") as url:
     s = url.read()
+    print(s)
 soup = BeautifulSoup(s, 'html.parser')
 
 for link in soup.find_all('a'):
@@ -17,14 +18,15 @@ for link in soup.find_all('a'):
         if '/news/2016-07' in filter:
             news_link.append('http://kathmandupost.ekantipur.com' + filter)
 
-new_title.append(list(set(news_link)))
 
-for i in range(70):
-    print(new_title[0][i])
+new_title.append(list(set(news_link)))
+# print(new_title)
+# for i in range(len(new_title[0])):
+#    print(new_title[0][i])
 
 
 # print(new_title)
-for i in range(1):
+for i in range(70):
     with urllib.request.urlopen(new_title[0][i]) as url:
         s = url.read()
     soup = BeautifulSoup(s, 'html.parser')
@@ -40,14 +42,23 @@ for i in range(1):
     #     #     writer = csv.writer(output, lineterminator='\n')
     #     #     writer.writerow(titles[i].string)
 
-    with open('text.txt', 'ab') as f:
-        for link in soup.find_all('p'):
-            f.write(str(link.string))
-    # for link in soup.find_all('p'):
-    #         print(link.string)
-    #         target = open("hellotxt", 'ab')
-    #         target.write(link.string)
-    #         target.close()
+
+    for link in soup.find_all('p'):
+        print(link.string)
+        target = open("hellotxt.csv", 'a')
+        target.write(str(link.string))
+        target.close()
+
+        with open('mycsv.csv', "w") as output:
+            writer = csv.writer(output, lineterminator='\n')
+            for val in str(link.string):
+                writer.writerow([val])
+
+    target = open("hellotxt.csv", 'a')
+    target.write("\n")
+    target.close()
+
+
 
             # with open('hello.txt', "w") as output:
             #     writer = csv.writer(output, lineterminator='\n')
@@ -55,3 +66,7 @@ for i in range(1):
             #     for val in link.string:
             #         writer.writerow([val])
 
+    # with open('hellotxt.csv','r') as file:
+    #     data = file.readlines()
+    #     for each in data:
+    #         print(each)
